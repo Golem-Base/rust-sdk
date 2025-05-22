ee5f068f5e26f637356bbf8b382a0df8c72b8e55
Yvan Sraka <yvan@sraka.xyz>
2025-05-22 20:01:25 +0200

    Add a pre-commit hook that does cargo fmt, clippy, audit, taplo, etc.

--- flake.nix
+++ flake.nix
@@ -62,85 +62,73 @@
 
         # Build the actual crate itself, reusing the dependency
         # artifacts from above.
-        my-crate = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-        });
-      in
-        {
-          checks = {
-            # Build the crate as part of `nix flake check` for convenience
-            inherit my-crate;
+        my-crate =
+          craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
+      in {
+        checks = {
+          # Build the crate as part of `nix flake check` for convenience
+          inherit my-crate;
 
-            # Run clippy (and deny all warnings) on the crate source,
-            # again, reusing the dependency artifacts from above.
-            #
-            # Note that this is done as a separate derivation so that
-            # we can block the CI if there are issues here, but not
-            # prevent downstream consumers from building our crate by itself.
-            my-crate-clippy = craneLib.cargoClippy (commonArgs // {
-              inherit cargoArtifacts;
-              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-            });
+          # Run clippy (and deny all warnings) on the crate source,
+          # again, reusing the dependency artifacts from above.
+          #
+          # Note that this is done as a separate derivation so that
+          # we can block the CI if there are issues here, but not
+          # prevent downstream consumers from building our crate by itself.
+          my-crate-clippy = craneLib.cargoClippy (commonArgs // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          });
 
-            my-crate-doc = craneLib.cargoDoc (commonArgs // {
-              inherit cargoArtifacts;
-            });
+          my-crate-doc =
+            craneLib.cargoDoc (commonArgs // { inherit cargoArtifacts; });
 
-            # Check formatting
-            my-crate-fmt = craneLib.cargoFmt {
-              inherit src;
-            };
+          # Check formatting
+          my-crate-fmt = craneLib.cargoFmt { inherit src; };
 
-            my-crate-toml-fmt = craneLib.taploFmt {
-              src = pkgs.lib.sources.sourceFilesBySuffices src [ ".toml" ];
-              # taplo arguments can be further customized below as needed
-              # taploExtraArgs = "--config ./taplo.toml";
-            };
-
-            # Audit dependencies
-            my-crate-audit = craneLib.cargoAudit {
-              inherit src advisory-db;
-            };
-
-            # Audit licenses
-            my-crate-deny = craneLib.cargoDeny {
-              inherit src;
-            };
-
-            # Run tests with cargo-nextest
-            # Consider setting `doCheck = false` on `my-crate` if you do not want
-            # the tests to run twice
-            my-crate-nextest = craneLib.cargoNextest (commonArgs // {
-              inherit cargoArtifacts;
-              partitions = 1;
-              partitionType = "count";
-              cargoNextestPartitionsExtraArgs = "--no-tests=pass";
-            });
+          my-crate-toml-fmt = craneLib.taploFmt {
+            src = pkgs.lib.sources.sourceFilesBySuffices src [ ".toml" ];
+            # taplo arguments can be further customized below as needed
+            # taploExtraArgs = "--config ./taplo.toml";
           };
 
-          packages = {
-            default = my-crate;
-          } // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
-            my-crate-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
-              inherit cargoArtifacts;
-            });
-          };
+          # Audit dependencies
+          my-crate-audit = craneLib.cargoAudit { inherit src advisory-db; };
 
-          apps.default = flake-utils.lib.mkApp {
-            drv = my-crate;
-          };
+          # Audit licenses
+          my-crate-deny = craneLib.cargoDeny { inherit src; };
 
-          devShells.default = craneLib.devShell {
-            # Inherit inputs from checks.
-            checks = self.checks.${system};
+          # Run tests with cargo-nextest
+          # Consider setting `doCheck = false` on `my-crate` if you do not want
+          # the tests to run twice
+          my-crate-nextest = craneLib.cargoNextest (commonArgs // {
+            inherit cargoArtifacts;
+            partitions = 1;
+            partitionType = "count";
+            cargoNextestPartitionsExtraArgs = "--no-tests=pass";
+          });
+        };
 
-            # Additional dev-shell environment variables can be set directly
-            # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+        packages = {
+          default = my-crate;
+        } // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+          my-crate-llvm-coverage = craneLibLLvmTools.cargoLlvmCov
+            (commonArgs // { inherit cargoArtifacts; });
+        };
 
-            # Extra inputs can be added here; cargo and rustc are provided by default.
-            packages = [
-              # pkgs.ripgrep
-            ];
-          };
-        });
+        apps.default = flake-utils.lib.mkApp { drv = my-crate; };
+
+        devShells.default = craneLib.devShell {
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
+
+          # Additional dev-shell environment variables can be set directly
+          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+
+          # Extra inputs can be added here; cargo and rustc are provided by default.
+          packages = [
+            pkgs.pre-commit
+          ];
+        };
+      });
 }
