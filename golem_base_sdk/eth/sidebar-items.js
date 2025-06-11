0fd44f2352773109845c61de114bde1cd211b880
yvan-sraka <yvan-sraka@users.noreply.github.com>
2025-06-11 10:53:40 +0000

    deploy: edbea318d956f6032a99f80dc1825a2be12d08ad

--- golem_base_sdk/eth/sidebar-items.js
+++ golem_base_sdk/eth/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["STORAGE_ADDRESS"],"enum":["Error"]};
+window.SIDEBAR_ITEMS = {"constant":["STORAGE_ADDRESS"],"enum":["Error"],"struct":["GolemBaseStorageEntityBTLExtended","GolemBaseStorageEntityCreated","GolemBaseStorageEntityDeleted","GolemBaseStorageEntityUpdated"]};
--- trait.impl/core/convert/trait.TryFrom.js
+++ trait.impl/core/convert/trait.TryFrom.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["golem_base_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;Log&gt; for <a class=\"enum\" href=\"golem_base_sdk/events/enum.Event.html\" title=\"enum golem_base_sdk::events::Event\">Event</a>"]]]]);
+    var implementors = Object.fromEntries([["golem_base_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;Log&gt; for <a class=\"enum\" href=\"golem_base_sdk/events/enum.Event.html\" title=\"enum golem_base_sdk::events::Event\">Event</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
