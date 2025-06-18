3e92ca247f1f83cb1f4988b31438c18fe7f7ffc5
r-vdp <r-vdp@users.noreply.github.com>
2025-06-18 16:17:18 +0000

    deploy: 4b80e63978dabba2209df2f4aece1ef0e83e8924

---
+++ rvdp/dev/src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["golem_base_sdk",["",[],["client.rs","entity.rs","eth.rs","events.rs","lib.rs","rpc.rs","utils.rs"]]],["golem_base_sdk_api",["",[],["api.rs","main.rs"]]],["golem_base_sdk_demo",["",[],["main.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[102,52,44]}
