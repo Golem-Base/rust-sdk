88f0e32823060371e750f165f9b5411c62ebe28b
eureka-cpu <eureka-cpu@users.noreply.github.com>
2025-08-14 18:07:37 +0000

    deploy: adf22932a9973351710bc06e1eaa8faac10360dc

--- trait.impl/core/fmt/trait.Display.js
+++ trait.impl/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["golem_base_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"golem_base_sdk/eth/enum.Error.html\" title=\"enum golem_base_sdk::eth::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"golem_base_sdk/rpc/enum.Error.html\" title=\"enum golem_base_sdk::rpc::Error\">Error</a>"]]]]);
+    var implementors = Object.fromEntries([["golem_base_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"golem_base_sdk/eth/enum.Error.html\" title=\"enum golem_base_sdk::eth::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"golem_base_sdk/rpc/enum.Error.html\" title=\"enum golem_base_sdk::rpc::Error\">Error</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
